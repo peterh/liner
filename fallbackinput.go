peterh/liner

commit 0e4af131b90a9786839c8b1b01717be263e8555a

Leave a column at the end of the line for the cursor
Windows 10, and MacOSX Terminal.app at the bottom of the window, treat the
cursor as a character when rewraping lines due to the resizing of the
terminal window. This results in garbage every time, not just when the
user resizes the window faster than Liner can react to SIGWINCH.

Fixes #71

input_windows.go
@@ -168,6 +168,9 @@ func (s *State) readNext() (interface{}, error) {
 		if input.eventType == window_buffer_size_event {
 			xy := (*coord)(unsafe.Pointer(&input.blob[0]))
 			s.columns = int(xy.x)
+			if s.columns > 1 {
+				s.columns--
+			}
 			return winch, nil
 		}
 		if input.eventType != key_event {

output.go
@@ -56,6 +56,9 @@ func (s *State) getColumns() bool {
 		return false
 	}
 	s.columns = int(ws.col)
+	if cursorColumn && s.columns > 1 {
+		s.columns--
+	}
 	return true
 }
 

output_windows.go
@@ -69,4 +69,8 @@ func (s *State) getColumns() {
 	var sbi consoleScreenBufferInfo
 	procGetConsoleScreenBufferInfo.Call(uintptr(s.hOut), uintptr(unsafe.Pointer(&sbi)))
 	s.columns = int(sbi.dwSize.x)
+	if s.columns > 1 {
+		// Windows 10 needs a spare column for the cursor
+		s.columns--
+	}
 }

bsdinput.go
@@ -37,3 +37,5 @@ type termios struct {
 	Ispeed int32
 	Ospeed int32
 }
+
+const cursorColumn = false

input_darwin.go
@@ -37,3 +37,7 @@ type termios struct {
 	Ispeed uintptr
 	Ospeed uintptr
 }
+
+// Terminal.app needs a column for the cursor when the input line is at the
+// bottom of the window.
+const cursorColumn = true

input_linux.go
@@ -24,3 +24,5 @@ const (
 type termios struct {
 	syscall.Termios
 }
+
+const cursorColumn = false

commit 6106ee4fe3e8435f18cd10e34557e5e50f0e792a

Fix for https://github.com/peterh/liner/issues/98
Changing the approach of fixing https://github.com/peterh/liner/issues/71
(introduced by 0e4af131b90a9786839c8b1b01717be263e8555a).

Instead of 'decreasing' screen width for cases when cursor which take extra
column, increase the buffer size.

With correct screen width multiline mode works properly.

fallbackinput.go
@@ -55,3 +55,5 @@ func (n noopMode) ApplyMode() error {
 func TerminalMode() (ModeApplier, error) {
 	return noopMode{}, nil
 }
+
+const cursorColumn = true

input_windows.go
@@ -189,9 +189,6 @@ func (s *State) readNext() (interface{}, error) {
 		if input.eventType == window_buffer_size_event {
 			xy := (*coord)(unsafe.Pointer(&input.blob[0]))
 			s.columns = int(xy.x)
-			if s.columns > 1 {
-				s.columns--
-			}
 			return winch, nil
 		}
 		if input.eventType != key_event {
@@ -363,3 +360,5 @@ func TerminalMode() (ModeApplier, error) {
 	}
 	return mode, err
 }
+
+const cursorColumn = true

line.go
@@ -113,6 +113,10 @@ func (s *State) refreshSingleLine(prompt []rune, buf []rune, pos int) error {
 
 	pLen := countGlyphs(prompt)
 	bLen := countGlyphs(buf)
+	// on some OS / terminals extra column is needed to place the cursor char
+	if cursorColumn {
+		bLen++
+	}
 	pos = countGlyphs(buf[:pos])
 	if pLen+bLen < s.columns {
 		_, err = fmt.Print(string(buf))
@@ -163,6 +167,14 @@ func (s *State) refreshSingleLine(prompt []rune, buf []rune, pos int) error {
 func (s *State) refreshMultiLine(prompt []rune, buf []rune, pos int) error {
 	promptColumns := countMultiLineGlyphs(prompt, s.columns, 0)
 	totalColumns := countMultiLineGlyphs(buf, s.columns, promptColumns)
+	// on some OS / terminals extra column is needed to place the cursor char
+	// if cursorColumn {
+	//	totalColumns++
+	// }
+
+	// it looks like Multiline mode always assume that a cursor need an extra column,
+	// and always emit a newline if we are at the screen end, so no worarounds needed there
+
 	totalRows := (totalColumns + s.columns - 1) / s.columns
 	maxRows := s.maxRows
 	if totalRows > s.maxRows {

output.go
@@ -56,9 +56,6 @@ func (s *State) getColumns() bool {
 		return false
 	}
 	s.columns = int(ws.col)
-	if cursorColumn && s.columns > 1 {
-		s.columns--
-	}
 	return true
 }
 

output_windows.go
@@ -69,8 +69,4 @@ func (s *State) getColumns() {
 	var sbi consoleScreenBufferInfo
 	procGetConsoleScreenBufferInfo.Call(uintptr(s.hOut), uintptr(unsafe.Pointer(&sbi)))
 	s.columns = int(sbi.dwSize.x)
-	if s.columns > 1 {
-		// Windows 10 needs a spare column for the cursor
-		s.columns--
-	}
 }