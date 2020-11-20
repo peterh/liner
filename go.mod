ab0ba8d048eb40a95136f81df29497661c90c1e0
Peter Harris <peter@harr.ca>
2020-11-19 22:46:15 -0500

    skip terminal width detection in PasswordPrompt
    There is no need to detect the terminal width for editing when there
    will be no editing.
    
    Incorrect detection can lead to PasswordPrompt echoing to the screen,
    which it should never do.
    
    Similarly, PasswordPrompt can never be in multiLineMode (because it
    never shows any part of an input line).
    
    Fixes #142 (introduced by 5c27c1d13d990c96fa371eaa519b2da3905570cf)

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/peterh/liner
 
+go 1.15
+
 require (
 	github.com/mattn/go-runewidth v0.0.3
 	golang.org/x/sys v0.0.0-20201110211018-35f3e6cf4a65
--- line.go
+++ line.go
@@ -1030,10 +1030,6 @@ func (s *State) PasswordPrompt(prompt string) (string, error) {
 	}
 
 	p := []rune(prompt)
-	const minWorkingSpace = 1
-	if s.columns < countGlyphs(p)+minWorkingSpace {
-		return s.tooNarrow(prompt)
-	}
 
 	defer s.stopPrompt()
 
@@ -1059,15 +1055,6 @@ mainLoop:
 		case rune:
 			switch v {
 			case cr, lf:
-				if s.needRefresh {
-					err := s.refresh(p, line, pos)
-					if err != nil {
-						return "", err
-					}
-				}
-				if s.multiLineMode {
-					s.resetMultiLine(p, line, pos)
-				}
 				fmt.Println()
 				break mainLoop
 			case ctrlD: // del
@@ -1095,9 +1082,6 @@ mainLoop:
 				}
 			case ctrlC:
 				fmt.Println("^C")
-				if s.multiLineMode {
-					s.resetMultiLine(p, line, pos)
-				}
 				if s.ctrlCAborts {
 					return "", ErrPromptAborted
 				}
